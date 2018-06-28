ff0a664569d33ef919a4457a275566e80ff266e6
GBTC441017UR <fadhld92@gmail.com>
2018-06-28 16:42:34 -0400

    Working bullhorn app, need to store likes in database

---
+++ src/main/java/com/example/demo/Reddit.java
@@ -0,0 +1,76 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+@Entity
+public class Reddit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotEmpty
+    @Size(min = 2)
+    private String name;
+
+    @NotEmpty
+    @Size(min = 2)
+    private String addressee;
+
+    @NotEmpty
+    @Size(min = 2)
+    private String title;
+//
+//    private int likes;
+
+    private LocalDateTime date;
+
+    public String getAddressee() {
+        return addressee;
+    }
+
+    public void setAddressee(String addressee) {
+        this.addressee = addressee;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDate() {
+        return date.format(DateTimeFormatter.ofPattern("MMMM dd, yyyy"));
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
